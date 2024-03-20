b73367de7ffeb27577ec2ff730e3a4b3292dca3f
Shkityr Konstantin <shkityr.k@yandex.ru>
2024-03-20 11:07:53 +0300

    raw UI

---
+++ app/src/main/java/com/example/samsungschoolproject/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.samsungschoolproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.samsungschoolproject.R;
+import com.example.samsungschoolproject.fragment.MainMenuFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().popBackStack();
+        MainMenuFragment mainMenuFragment = new MainMenuFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, mainMenuFragment);
+        transaction.commit();
+    }
+}
