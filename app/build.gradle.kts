0451be046b4b47f7c213acefc67d1de7836fae10
Shkityr Konstantin <shkityr.k@yandex.ru>
2024-07-12 21:59:21 +0300

    refactor: dependences build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,38 +32,34 @@ android {
 }
 
 dependencies {
+    // UI Components
+    implementation("androidx.appcompat:appcompat:1.6.1") // Support for ActionBar, themed Activities, and compatibility with older Android versions
+    implementation("com.google.android.material:material:1.11.0") // Material Design components like buttons, cards, etc.
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4") // Layout for creating flexible and responsive UI
+    implementation("androidx.recyclerview:recyclerview:1.3.0") // Support for RecyclerView, a flexible view for providing a limited window into large data sets
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
-    implementation("androidx.activity:activity:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("org.slf4j:slf4j-api:2.0.12")
-    implementation("org.slf4j:slf4j-simple:1.7.32")
+    // Legacy Support
+    implementation("androidx.legacy:legacy-support-v4:1.0.0") // Support library for older Android versions
 
+    // Activity
+    implementation("androidx.activity:activity:1.8.0") // Support for Activity class and component lifecycle
+
+    // Testing
+    testImplementation("junit:junit:4.13.2") // JUnit testing framework
+    androidTestImplementation("androidx.test.ext:junit:1.1.5") // JUnit extensions for Android testing
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1") // Espresso for UI testing
+
+    // Room Database
     val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion") // Room runtime
+    annotationProcessor("androidx.room:room-compiler:$roomVersion") // Room annotation processor for code generation
 
-    implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
-    implementation("org.xerial:sqlite-jdbc:3.45.2.0")
+    // SQLite
+    implementation("org.xerial:sqlite-jdbc:3.45.2.0") // SQLite JDBC driver
 
-    val lifecycleVersion = "'2.3.1'"
+    // Google Play Services
+    implementation("com.google.android.gms:play-services-location:18.0.0") // Google Play services for location
 
-    // Lifecycle components
-    implementation ("androidx.lifecycle:lifecycle-viewmodel:$lifecycleVersion")
-    implementation ("androidx.lifecycle:lifecycle-livedata:$lifecycleVersion")
-    implementation ("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
-
-    //dotenv
-
-    implementation ("io.github.cdimascio:java-dotenv:5.2.2")
-
-    implementation("androidx.core:core-splashscreen:1.0.1")
-
-    implementation ("com.google.android.gms:play-services-location:18.0.0")
-
+    // JSON Parsing
+    implementation("com.google.code.gson:gson:2.8.7") // Gson library for JSON parsing
 }
