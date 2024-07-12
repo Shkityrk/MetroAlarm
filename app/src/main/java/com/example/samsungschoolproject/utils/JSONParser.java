6827f082dae3b5d39c5b31d8b28b121bfc24d07f
Shkityr Konstantin <shkityr.k@yandex.ru>
2024-07-12 21:59:57 +0300

    refactor: JSON parcer use gson library

--- app/src/main/java/com/example/samsungschoolproject/utils/JSONParser.java
+++ app/src/main/java/com/example/samsungschoolproject/utils/JSONParser.java
@@ -1,10 +1,11 @@
 package com.example.samsungschoolproject.utils;
 
 import com.example.samsungschoolproject.model.Station;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,31 +13,14 @@ public class JSONParser {
 
     public static List<Station> stationsParseJSON(String jsonString) {
         List<Station> stationList = new ArrayList<>();
-        try {
-            JSONArray jsonArray = new JSONArray(jsonString);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                int stationId = jsonObject.getInt("id_station");
-                String name = jsonObject.getString("name");
-                String line = jsonObject.getString("line");
-                String latitude = jsonObject.getString("latitude");
-                String longitude = jsonObject.getString("longitude");
-                String widthNeighbour1 = jsonObject.optString("width_neighbour1", null);
-                String longitudeNeighbour1 = jsonObject.optString("longitude_neighbour1", null);
-                String widthNeighbour2 = jsonObject.optString("width_neighbour2", null);
-                String longitudeNeighbour2 = jsonObject.optString("longitude_neighbour2", null);
-                String isFavourite = jsonObject.getString("is_favourite");
-                String alarm = jsonObject.getString("alarm");
+        Gson gson = new Gson();
 
-                Station station = new Station(name, line, latitude, longitude,
-                        widthNeighbour1, longitudeNeighbour1, widthNeighbour2,
-                        longitudeNeighbour2, isFavourite, alarm);
-                station.setId(stationId);
-                stationList.add(station);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+        JsonArray jsonArray = JsonParser.parseString(jsonString).getAsJsonArray();
+        for (JsonElement jsonElement : jsonArray) {
+            Station station = gson.fromJson(jsonElement, Station.class);
+            stationList.add(station);
         }
+
         return stationList;
     }
 }
